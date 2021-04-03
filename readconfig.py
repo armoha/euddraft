b6dc62751568757944ca3ab3b83c4f343f71cf3d
armoha <kein0011@naver.com>
2021-04-03 20:30:23 +0900

    [edd] `::Comment` syntax, Check duplicated key

--- readconfig.py
+++ readconfig.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 Copyright (c) 2014 trgk
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,19 +21,19 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-'''
+"""
 
 import re
 from collections import OrderedDict
 
 
 def readconfig(fname):
-    s = open(fname, 'rb').read()
+    s = open(fname, "rb").read()
 
     try:
-        s = s.decode('cp949')
+        s = s.decode("cp949")
     except UnicodeDecodeError:
-        s = s.decode('utf-8')
+        s = s.decode("utf-8")
 
     currentSectionName = None
     currentSection = None
@@ -43,7 +43,7 @@ def readconfig(fname):
     keyvalue_regex = re.compile(r"(([^\\:=]|\\.)+)\s*[:=]\s*(.+)$")
     keyonly_regex = re.compile(r"(([^\\:=]|\\.)+)")
     key_replace_regex = re.compile(r"\\([\\:=])")
-    comment_regex = re.compile(r"[;#].*")
+    comment_regex = re.compile(r"::.*")
 
     lines = s.splitlines()
     for line in lines:
@@ -52,39 +52,45 @@ def readconfig(fname):
             continue
 
         # Try header
-        header_match = header_regex.match(line)
+        header_match = header_regex.fullmatch(line)
         if header_match:
             currentSectionName = header_match.group(1)
             if currentSectionName in config:
-                raise RuntimeError('Duplicate header %s' % currentSectionName)
+                raise RuntimeError("Duplicate header %s" % currentSectionName)
             currentSection = {}
             config[currentSectionName] = currentSection
             continue
 
         # Try key-value pair
-        keyvalue_match = keyvalue_regex.match(line)
+        keyvalue_match = keyvalue_regex.fullmatch(line)
         if keyvalue_match:
             key = keyvalue_match.group(1)
             key = key_replace_regex.sub(r"\1", key)
             key = key.strip()
             value = keyvalue_match.group(3)
+            if key in currentSection:
+                raise RuntimeError("Duplicate key %s" % key)
             currentSection[key] = value
             continue
 
         # Try key-only pair
-        keyonly_match = keyonly_regex.match(line)
+        keyonly_match = keyonly_regex.fullmatch(line)
         if keyonly_match:
             key = keyonly_match.group(1)
             key = key_replace_regex.sub(r"\1", key)
-            key = key.replace('\\:', ':')
-            key = key.replace('\\=', '=')
+            key = key.replace("\\:", ":")
+            key = key.replace("\\=", "=")
             key = key.strip()
-            currentSection[key] = ''
+            if key in currentSection:
+                raise RuntimeError("Duplicate key %s" % key)
+            currentSection[key] = ""
             continue
 
         # Try comment
         comment_match = comment_regex.match(line)
         if comment_match:
             continue
+
+        raise RuntimeError("Invalid config %s" % line)
 
     return config
