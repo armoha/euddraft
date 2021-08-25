38543368deffe4664f2cf85b3fe1fd9660850004
armoha <kein0011@naver.com>
2021-08-26 01:21:00 +0900

    euddraft 0.9.3.1

--- readconfig.py
+++ readconfig.py
@@ -29,90 +29,92 @@ from typing import Dict
 
 
 def readconfig(fname) -> Dict[str, Dict[str, str]]:
-    try:
-        text = open(fname)
-    except UnicodeDecodeError:
-        text = open(fname, encoding="UTF-8")
-
-    currentSectionName = None
-    currentSection = None
-    config = OrderedDict()
-
     header_regex = re.compile(r"\[(.+)\]$")
     keyvalue_regex = re.compile(r"(([^\\:=]|\\.)+)\s*[:=]\s*(.+)$")
     keyonly_regex = re.compile(r"(([^\\:=]|\\.)+)")
     key_replace_regex = re.compile(r"\\([\\:=])")
     comment_regex = re.compile(r"::.*")
 
-    header_lineno = {}
-    config_lineno = {}
+    def parse_config(text):
+        currentSectionName = None
+        currentSection = None
+        config = OrderedDict()
+        header_lineno = {}
+        config_lineno = {}
+        for lineno, line in enumerate(text):
+            lineno += 1
+            line = line.strip()
+            if not line:  # empty line
+                continue
 
-    for lineno, line in enumerate(text):
-        lineno += 1
-        line = line.strip()
-        if not line:  # empty line
-            continue
+            # Try header
+            header_match = header_regex.fullmatch(line)
+            if header_match:
+                currentSectionName = header_match.group(1)
+                if currentSectionName in config:
+                    raise RuntimeError(
+                        "Duplicate header [%s]" % currentSectionName,
+                        "[Line %u] [%s]"
+                        % (header_lineno[currentSectionName], currentSectionName),
+                        "[Line %u] [%s]" % (lineno, currentSectionName),
+                    )
+                currentSection = {}
+                config[currentSectionName] = currentSection
+                header_lineno[currentSectionName] = lineno
+                config_lineno.clear()
+                continue
 
-        # Try header
-        header_match = header_regex.fullmatch(line)
-        if header_match:
-            currentSectionName = header_match.group(1)
-            if currentSectionName in config:
-                raise RuntimeError(
-                    "Duplicate header [%s]" % currentSectionName,
-                    "[Line %u] [%s]"
-                    % (header_lineno[currentSectionName], currentSectionName),
-                    "[Line %u] [%s]" % (lineno, currentSectionName),
-                )
-            currentSection = {}
-            config[currentSectionName] = currentSection
-            header_lineno[currentSectionName] = lineno
-            config_lineno.clear()
-            continue
+            # Try key-value pair
+            keyvalue_match = keyvalue_regex.fullmatch(line)
+            if keyvalue_match:
+                key = keyvalue_match.group(1)
+                key = key_replace_regex.sub(r"\1", key)
+                key = key.strip()
+                value = keyvalue_match.group(3)
+                if key in currentSection:
+                    raise RuntimeError(
+                        "Duplicate key %s" % key,
+                        "[Line %u] %s : %s"
+                        % (config_lineno[key], key, currentSection[key]),
+                        "[Line %u] %s : %s" % (lineno, key, value),
+                    )
+                currentSection[key] = value
+                config_lineno[key] = lineno
+                continue
 
-        # Try key-value pair
-        keyvalue_match = keyvalue_regex.fullmatch(line)
-        if keyvalue_match:
-            key = keyvalue_match.group(1)
-            key = key_replace_regex.sub(r"\1", key)
-            key = key.strip()
-            value = keyvalue_match.group(3)
-            if key in currentSection:
-                raise RuntimeError(
-                    "Duplicate key %s" % key,
-                    "[Line %u] %s : %s"
-                    % (config_lineno[key], key, currentSection[key]),
-                    "[Line %u] %s : %s" % (lineno, key, value),
-                )
-            currentSection[key] = value
-            config_lineno[key] = lineno
-            continue
+            # Try key-only pair
+            keyonly_match = keyonly_regex.fullmatch(line)
+            if keyonly_match:
+                key = keyonly_match.group(1)
+                key = key_replace_regex.sub(r"\1", key)
+                key = key.replace("\\:", ":")
+                key = key.replace("\\=", "=")
+                key = key.strip()
+                if key in currentSection:
+                    raise RuntimeError(
+                        "Duplicate key %s in [%s]" % (key, currentSectionName),
+                        "[Line %u] %s : %s"
+                        % (config_lineno[key], key, currentSection[key]),
+                        "[Line %u] %s" % (lineno, key),
+                    )
+                currentSection[key] = ""
+                config_lineno[key] = lineno
+                continue
 
-        # Try key-only pair
-        keyonly_match = keyonly_regex.fullmatch(line)
-        if keyonly_match:
-            key = keyonly_match.group(1)
-            key = key_replace_regex.sub(r"\1", key)
-            key = key.replace("\\:", ":")
-            key = key.replace("\\=", "=")
-            key = key.strip()
-            if key in currentSection:
-                raise RuntimeError(
-                    "Duplicate key %s in [%s]" % (key, currentSectionName),
-                    "[Line %u] %s : %s"
-                    % (config_lineno[key], key, currentSection[key]),
-                    "[Line %u] %s" % (lineno, key),
-                )
-            currentSection[key] = ""
-            config_lineno[key] = lineno
-            continue
+            # Try comment
+            comment_match = comment_regex.match(line)
+            if comment_match:
+                continue
 
-        # Try comment
-        comment_match = comment_regex.match(line)
-        if comment_match:
-            continue
+            raise RuntimeError("Invalid config %s" % line)
+        return config
 
-        raise RuntimeError("Invalid config %s" % line)
+    try:
+        text = open(fname, encoding="UTF-8")
+        config = parse_config(text)
+    except UnicodeDecodeError:
+        text = open(fname)
+        config = parse_config(text)
 
     text.close()
     return config
